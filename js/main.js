3a644bdc78ba054590d289d1e63ac1a7da31ff6e
Vladislav <eclipsik69@gmail.com>
2019-10-23 20:12:49 +0300

    desktop v2

--- js/main.js
+++ js/main.js
@@ -1,35 +1,89 @@
-function sliderInit(slider, toShow) {
-    $(slider).slick({
-        infinite: true,
-        dots: false,
-        slidesToShow: toShow,
-        slidesToScroll: toShow,
-        arrows: false,
-        autoplay: false
+$(document).ready(function () {
+    $('.lang').click(function () {
+        if($(this).hasClass('selected')) {
+            return
+        } else {
+            $('.lang').each(function () {
+                $(this).removeClass('selected')
+            })
+            $(this).addClass('selected')
+        }
     })
-}
 
-sliderInit('.slider_stage', 2);
-sliderInit('.slider_team', 1);
 
-function sliderControl(element,direction) {
-    $(element).slick('slick' + direction);
-}
 
-$('.prev').click(function () {
-    sliderControl('.slider_stage','Prev')
+    $('.form').submit(function (e) {
+        e.preventDefault();
+        var inputs = $(this).find('input');
+        let errObj = {
+            0: 'Це поле не можу бути пустим!',
+            1: 'Перевірте правильність вводу!'
+        };
+
+        for(let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+            if(input.value.length < 1) {
+                input.nextElementSibling.classList.add('show');
+                input.nextElementSibling.textContent = errObj[0];
+                input.classList.add('err')
+            }
+            if(input.name === 'name') {
+                if(input.value.length < 1) {
+                    input.nextElementSibling.classList.add('show');
+                    input.nextElementSibling.textContent = errObj[0];
+                    input.classList.add('err')
+                } else {
+                    input.nextElementSibling.classList.remove('show');
+                    input.nextElementSibling.textContent = errObj[0];
+                    input.classList.remove('err')
+                }
+            }
+            if(input.name === 'mail') {
+                let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                if(re.test(input.value)) {
+                    input.classList.remove('err');
+                    input.nextElementSibling.classList.remove('show');
+                    input.nextElementSibling.textContent = '';
+                } else {
+                    input.nextElementSibling.textContent = errObj[1];
+                }
+            }
+            if(input.name === 'phone') {
+                let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+                if(re.test(input.value)) {
+                    input.classList.remove('err');
+                    input.nextElementSibling.textContent = '';
+                    input.nextElementSibling.classList.remove('show');
+                } else {
+                    input.nextElementSibling.classList.add('show');
+                    input.nextElementSibling.textContent = errObj[1];
+                }
+            }
+        }
+    })
+
+    function targetedScroll(id) {
+        var scrollTop = id ? $('#' + id).offset().top : 0;
+
+        $('body,html').animate({
+            scrollTop: scrollTop,
+        }, 500);
+    }
+
+    function sliderInit(slider, toShow) {
+        $(slider).slick({
+            infinite: true,
+            dots: false,
+            slidesToShow: toShow,
+            slidesToScroll: toShow,
+            arrows: false,
+            autoplay: false
+        })
+    }
+
+    sliderInit('.slider_stage', 2);
+    sliderInit('.slider_team', 1);
+
+
+    AOS.init();
 })
-
-$('.next').click(function () {
-    sliderControl('.slider_stage','Next')
-})
-
-$('.prev_tm').click(function () {
-    sliderControl('.slider_team','Prev')
-})
-
-$('.next_tm').click(function () {
-    sliderControl('.slider_team','Next')
-})
-
-AOS.init();
