a438c579599fbe6d260485b138312161873b953e
Vladislav <eclipsik69@gmail.com>
2019-10-27 20:29:35 +0200

    finav v0.1

--- js/main.js
+++ js/main.js
@@ -12,63 +12,104 @@ $(document).ready(function () {
 
 
 
+    // $('.form').submit(function (e) {
+    //     e.preventDefault();
+    //     var inputs = $(this).find('input');
+    //     let errObj = {
+    //         0: 'Це поле не можу бути пустим!',
+    //         1: 'Перевірте правильність вводу!'
+    //     };
+    //
+    //     for(let i = 0; i < inputs.length; i++) {
+    //         let input = inputs[i];
+    //         if(input.value.length < 1) {
+    //             input.nextElementSibling.classList.add('show');
+    //             input.nextElementSibling.textContent = errObj[0];
+    //             input.classList.add('err')
+    //         }
+    //         if(input.name === 'name') {
+    //             if(input.value.length < 1) {
+    //                 input.nextElementSibling.classList.add('show');
+    //                 input.nextElementSibling.textContent = errObj[0];
+    //                 input.classList.add('err')
+    //             } else {
+    //                 input.nextElementSibling.classList.remove('show');
+    //                 input.nextElementSibling.textContent = errObj[0];
+    //                 input.classList.remove('err')
+    //             }
+    //         }
+    //         if(input.name === 'mail') {
+    //             let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //             if(re.test(input.value)) {
+    //                 input.classList.remove('err');
+    //                 input.nextElementSibling.classList.remove('show');
+    //                 input.nextElementSibling.textContent = '';
+    //             } else {
+    //                 input.nextElementSibling.textContent = errObj[1];
+    //             }
+    //         }
+    //         if(input.name === 'phone') {
+    //             let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+    //             if(re.test(input.value)) {
+    //                 input.classList.remove('err');
+    //                 input.nextElementSibling.textContent = '';
+    //                 input.nextElementSibling.classList.remove('show');
+    //             } else {
+    //                 input.nextElementSibling.classList.add('show');
+    //                 input.nextElementSibling.textContent = errObj[1];
+    //             }
+    //         }
+    //     }
+    // })
+
     $('.form').submit(function (e) {
         e.preventDefault();
-        var inputs = $(this).find('input');
+        let inputs = $(this).find('input');
+        let errList = [];
+        formValidation(inputs, errList)
+    });
+
+    function formValidation(inputs, errList) {
         let errObj = {
             0: 'Це поле не можу бути пустим!',
             1: 'Перевірте правильність вводу!'
         };
 
         for(let i = 0; i < inputs.length; i++) {
-            let input = inputs[i];
-            if(input.value.length < 1) {
-                input.nextElementSibling.classList.add('show');
-                input.nextElementSibling.textContent = errObj[0];
-                input.classList.add('err')
-            }
-            if(input.name === 'name') {
-                if(input.value.length < 1) {
-                    input.nextElementSibling.classList.add('show');
-                    input.nextElementSibling.textContent = errObj[0];
-                    input.classList.add('err')
-                } else {
-                    input.nextElementSibling.classList.remove('show');
-                    input.nextElementSibling.textContent = errObj[0];
-                    input.classList.remove('err')
+            if(inputs[i].value.length < 1) {
+                $(inputs[i]).next().text(errObj[0]).addClass('show');
+                $(inputs[i]).addClass('err');
+                errList.push('lengthErr')
+            } else {
+                if(inputs[i].name === 'mail') {
+                    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    if(re.test(inputs[i].value)) {
+                        $(inputs[i]).removeClass('err');
+                        $(inputs[i]).next().text('').removeClass('show');
+                    } else {
+                        $(inputs[i]).addClass('err');
+                        $(inputs[i]).next().text(errObj[1]).addClass('show');
+                        errList.push('mailErr')
+                    }
                 }
-            }
-            if(input.name === 'mail') {
-                let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                if(re.test(input.value)) {
-                    input.classList.remove('err');
-                    input.nextElementSibling.classList.remove('show');
-                    input.nextElementSibling.textContent = '';
-                } else {
-                    input.nextElementSibling.textContent = errObj[1];
-                }
-            }
-            if(input.name === 'phone') {
-                let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-                if(re.test(input.value)) {
-                    input.classList.remove('err');
-                    input.nextElementSibling.textContent = '';
-                    input.nextElementSibling.classList.remove('show');
-                } else {
-                    input.nextElementSibling.classList.add('show');
-                    input.nextElementSibling.textContent = errObj[1];
+                if(inputs[i].name === 'phone') {
+                    let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+                    if(re.test(inputs[i].value)) {
+                        $(inputs[i]).removeClass('err');
+                        $(inputs[i]).next().text('').removeClass('show');
+                    } else {
+                        $(inputs[i]).addClass('err');
+                        $(inputs[i]).next().text(errObj[1]).addClass('show');
+                        errList.push('phoneErr')
+                    }
                 }
             }
         }
-    })
-
-    function targetedScroll(id) {
-        var scrollTop = id ? $('#' + id).offset().top : 0;
-
-        $('body,html').animate({
-            scrollTop: scrollTop,
-        }, 500);
+        if(errList.length === 0) {
+            console.log('can be send')
+        }
     }
+
 
     function sliderInit(slider, toShow) {
         $(slider).slick({
@@ -77,13 +118,38 @@ $(document).ready(function () {
             slidesToShow: toShow,
             slidesToScroll: toShow,
             arrows: false,
-            autoplay: false
+            autoplay: false,
+            responsive: [
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         })
     }
 
     sliderInit('.slider_stage', 2);
     sliderInit('.slider_team', 1);
-
-
+    
     AOS.init();
+    
+    function menuToggle() {
+        let mnuBtn = $('.menu_btn'),
+            mnu = $('.right_block'),
+            root = $('.root');
+        mnuBtn.click(function () {
+            if(mnu.hasClass('open')) {
+                mnu.removeClass('open')
+                root.removeClass('blurred')
+            } else {
+                mnu.addClass('open')
+                root.addClass('blurred')
+            }
+        })
+    }
+
+    menuToggle();
 })
